1eb035a4c4a0c0b8b6355d111fb5561e488db2f8
wungthing <wungthingkashungnao123@gmail.com>
2023-09-24 14:07:00 +0530

    connecting to database

---
+++ db/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB database!");
+  } catch (error) {
+    throw error;
+  }
+};
+
+export default connect;
--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+// local file import start
+import connect from "./db/db.js";
+// local file import end
 
 const app = express();
+dotenv.config(); //configuring dotenv so we can access variables from .env file
+
+// this code run when we disconnect or connect again with the db after the initial connection
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDb disconnected");
+});
+mongoose.connection.on("connected", () => {
+  console.log("mongoDb connected");
+});
+
 app.listen(3000, () => {
+  connect();
   console.log("server has started on port 3000");
 });
